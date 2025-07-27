bc14fc21a99b606bdc4648c2fb50d5d9883f670a
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-07-27 13:04:37 +0300

    Change side bar Color And Width

--- mypreset.ts
+++ mypreset.ts
@@ -2,40 +2,27 @@ import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 
 export const MyPreset = definePreset(Aura, {
-  //Your customizations, see the following sections for examples
-  semantic: {
-    colorScheme: {
-      light: {
-        surface: {
-          0: '#ffffff',
-          50: '{white.50}',
-          100: '{zinc.100}',
-          200: '{zinc.200}',
-          300: '{zinc.300}',
-          400: '{zinc.400}',
-          500: '{zinc.500}',
-          600: '{zinc.600}',
-          700: '{zinc.700}',
-          800: '{zinc.800}',
-          900: '{zinc.900}',
-          950: '{zinc.950}',
+  components: {
+    drawer: {
+      colorScheme: {
+        light: {
+          title: {
+            fontWeight: 'bold',
+          },
+          root: {
+            background: 'white',
+            color: 'black',
+          },
+        },
+        dark: {
+          root: {
+            background: '#101828',
+            color: 'white',
+          },
         },
       },
-      dark: {
-        surface: {
-          0: '#ffffff',
-          50: '{slate.50}',
-          100: '{slate.100}',
-          200: '{slate.200}',
-          300: '{slate.300}',
-          400: '{slate.400}',
-          500: '{slate.500}',
-          600: '{slate.600}',
-          700: '{slate.700}',
-          800: '{slate.800}',
-          900: '{slate.900}',
-          950: '{slate.950}',
-        },
+      root: {
+        borderColor: 'transparent',
       },
     },
   },
